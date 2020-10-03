e99e4a95a00612345fcaa018e84b5b992403cb7f
Shifatur-Rahman <rjnovel28@gmail.com>
2020-10-03 19:50:05 +0600

    Updates

---
+++ precache-manifest.c737fadcf7635b1792e4cc500344469f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b666e84f66c9d40826c571570e1c1f",
+    "url": "/e-commerce-app/index.html"
+  },
+  {
+    "revision": "0853f9a9e187518c99bf",
+    "url": "/e-commerce-app/static/css/main.acaa70fb.chunk.css"
+  },
+  {
+    "revision": "a2e7b18e15643213c810",
+    "url": "/e-commerce-app/static/js/2.e955e3ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/e-commerce-app/static/js/2.e955e3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0853f9a9e187518c99bf",
+    "url": "/e-commerce-app/static/js/main.27870c13.chunk.js"
+  },
+  {
+    "revision": "098b082849cdb7045b0e",
+    "url": "/e-commerce-app/static/js/runtime-main.c1e94fce.js"
+  },
+  {
+    "revision": "ebda9397a46c28cf97f0ea60305fa7c0",
+    "url": "/e-commerce-app/static/media/cloth_slider.ebda9397.jpg"
+  },
+  {
+    "revision": "ba6552f5bcc32f2d3bbc830e5f67afda",
+    "url": "/e-commerce-app/static/media/product_two.ba6552f5.jpg"
+  },
+  {
+    "revision": "cb231778a08aa76141c9cda41344133d",
+    "url": "/e-commerce-app/static/media/sharee_one.cb231778.jpg"
+  }
+]);
